ea4b362476ef5e082fdf3ba0a415793c383e0832
Ali Moussa <imalimoussa@gmail.com>
2020-06-01 14:50:04 +0200

    minor stuff

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     const divEl = $('#trdiv');
     const bottom_row = $('#navbar-bottom-row');
     const milliseconds = 150;
-    
+
     const collapseOnResize = () => {
         if (divEl.is(':visible')) {
             bottom_row.collapse('hide');
@@ -10,15 +10,14 @@ $(document).ready(function () {
             bottom_row.collapse('show');
         }
     }
-    
+
     // call on init in case window was opened initially in a small screen
-    collapseOnResize(); 
-    
-    
+    collapseOnResize();
+
     // window resize event
     let resizeTimer;
     $(window).on('resize', (e) => {
-      clearTimeout(resizeTimer);
-      resizeTimer = setTimeout(collapseOnResize, milliseconds);
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(collapseOnResize, milliseconds);
     });
-  });
+});
