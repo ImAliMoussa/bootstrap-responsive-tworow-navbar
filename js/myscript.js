f0fcc90d459367cc76eaa20e21072af965ad42cd
Ali Moussa <imalimoussa@gmail.com>
2020-06-01 10:51:17 +0200

    cleaning up

--- js/myscript.js
+++ js/myscript.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
-    let resizeTimer;
     const divEl = $('#trdiv');
     const bottom_row = $('#navbar-bottom-row');
     const milliseconds = 150;
-
+    
     const collapseOnResize = () => {
-      console.log('function is called');
-      if (divEl.is(':visible')) {
-        bottom_row.collapse('hide');
-      } else {
-        bottom_row.collapse('show');
-      }
+        if (divEl.is(':visible')) {
+            bottom_row.collapse('hide');
+        } else {
+            bottom_row.collapse('show');
+        }
     }
-    $(window).on('resize', function (e) {
+    
+    
+    // call on init in case window was opened initially in a small screen
+    collapseOnResize(); 
+    
+    
+    // window resize event
+    let resizeTimer;
+    $(window).on('resize', (e) => {
       clearTimeout(resizeTimer);
       resizeTimer = setTimeout(collapseOnResize, milliseconds);
     });
